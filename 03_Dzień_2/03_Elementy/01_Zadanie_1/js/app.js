69de49b279035e4ced687f03a686a0c56642fe41
TheRizzy <dawidjanic1@gmail.com>
2023-05-21 12:07:02 +0200

    day 2 - elements task 1,2

--- 03_Dzień_2/03_Elementy/01_Zadanie_1/js/app.js
+++ 03_Dzień_2/03_Elementy/01_Zadanie_1/js/app.js
@@ -1,1 +1,15 @@
 const links = document.querySelector(".links").querySelectorAll("li");
+
+const links = document.querySelector('.links').querySelectorAll('li');
+
+function getDataInfo(elements) {
+  const result = [];
+
+  elements.forEach(function (element) {
+    result.push(element.dataset.color);
+  });
+
+  return result;
+}
+
+console.log(getDataInfo(links));
--- 03_Dzień_2/03_Elementy/02_Zadanie_2/js/app.js
+++ 03_Dzień_2/03_Elementy/02_Zadanie_2/js/app.js
@@ -3,3 +3,19 @@ const children = document.querySelector(".offers").querySelectorAll("div, h2, p"
 const ban = document.querySelector(".ban");
 const someBlocks = document.querySelectorAll(".block");
 const listElements = document.querySelector(".links").querySelectorAll("li");
+
+console.log(home); // 
+console.log(children); //pesudotablica
+children.forEach(oneChildren => {
+    console.log(oneChildren);
+});
+console.log(ban); //
+console.log(someBlocks); //pseudotablica
+someBlocks.forEach(block => {
+    console.log("Block from list someBlocks:",block);
+});
+console.log(listElements); //pseudotablica
+listElements.forEach(element => {
+    console.log("One element from list:", element)
+})
+
